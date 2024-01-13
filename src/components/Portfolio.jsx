465dfa360a20fc76044d60b1be1376c3ac99a3e7
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-01-13 18:48:32 +0100

    feat: rearrange project sections
    Moved the project sections to the bottom of the portfolio page.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PortfolioItem from "./PortfolioItem";
-import { iOSData, ReactData } from "./data/portfolio";
+import { ReactData, iOSData } from "./data/portfolio";
 
 function Portfolio({ handleButtonClick }) {
-  const data = handleButtonClick === "iOS" ? iOSData : ReactData;
+  const data = handleButtonClick === "iOS" ? ReactData : iOSData;
   return (
     <div className="flex flex-col md:flex-row items-center justify-center">
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
