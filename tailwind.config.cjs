8b2a4ed6812a39d09706e87a191bfbaa26ca55dc
Surya Teja G <gsuryateja05@gmail.com>
2024-01-20 01:51:21 +0530

    feat: added padding between each Timeline component and added custom Tailwind CSS variant to enable adding css to non last child in a container

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,33 @@
 /**  @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{jsx,js}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        Oswald: ["Oswald", "serif"],
-        SourceCodePro: ["Source Code Pro", "monospace", "sans-serif"],
-      },
-      backgroundColor: {
-        "green-300": "#556B2F",
-        "swift-300": "#FA7343",
-        "react-300": "#68DBFB",
-        "onyx-300": "#353839",
-        "sage-300": "#92AFAD",
-      },
+    content: ["./index.html", "./src/**/*.{jsx,js}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            fontFamily: {
+                Oswald: ["Oswald", "serif"],
+                SourceCodePro: ["Source Code Pro", "monospace", "sans-serif"],
+            },
+            backgroundColor: {
+                "green-300": "#556B2F",
+                "swift-300": "#FA7343",
+                "react-300": "#68DBFB",
+                "onyx-300": "#353839",
+                "sage-300": "#92AFAD",
+            },
+        },
     },
-  },
-  plugins: [],
+    //a custom variant to check if the element is the last child within its parent container
+    plugins: [
+        function ({ addVariant, e }) {
+            addVariant("not-last-child", ({ modifySelectors, separator }) => {
+                modifySelectors(({ className }) => {
+                    return `.${e(
+                        `not-last-child${separator}${className}`
+                    )}:not(:last-child)`;
+                });
+            });
+        },
+    ],
 };
